f84af9efc4fb080b958c13136524d5f643e206c2
RayanSf <rayansf18@gmail.com>
2024-02-10 09:59:16 -0300

    Todo repository

---
+++ src/main/java/com/rayan/Todo/repositorie/TodoRepository.java
@@ -0,0 +1,8 @@
+package com.rayan.Todo.repositorie;
+
+import com.rayan.Todo.domain.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {}
